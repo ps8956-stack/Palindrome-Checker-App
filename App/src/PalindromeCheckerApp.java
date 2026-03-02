f3e01f2a822aefbfd25896b5f7f73a303b6af9cd
ps8956-stack <ps8956@srmist.edu.in>
2026-03-02 09:52:02 +0530

    Prithvi added UseCase1

---
+++ App/src/PalindromeCheckerApp.java
@@ -0,0 +1,9 @@
+public class PalindromeCheckerApp {
+    public static void useCase1(){
+        System.out.println("Welcome to Palindrome Checker App Management System");
+        System.out.println("Version: 1.0");
+    }
+    public static void main(String args[]){
+        useCase1();
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Palindrome-Checker-App
+//objective: usecase1
+//flow of project: display name and version of app
+/procedure: create function useCase1(). add code to print name and version
+//topics covered: branch, commit, push, pull
